ab2f3b71282382b4665f14a70c4d5a9b89449571
GustavoGGF <gustavoguilhermedefreitas@gmail.com>
2026-03-27 09:40:00 -0300

    refactor(app): models.py
    Rename trasacoes table to transacoes and implement users entity

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 from sqlalchemy import Column, Integer, Date, Numeric, String, Time
 from .database import Base
+from sqlalchemy.orm import Mapped, mapped_column
 
 class Transacao(Base):
-    __tablename__ = "trasacoes"
+    __tablename__ = "transacoes"
     
     id = Column(Integer, primary_key=True, index=True)
     tipo = Column(Integer, nullable=False)
@@ -13,3 +14,9 @@ class Transacao(Base):
     hora = Column(Time, nullable=False)
     dono_loja = Column(String(14), nullable=False)
     nome_loja = Column(String(19), nullable=False)
+    
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    username: Mapped[str] = mapped_column(String, unique=True, index=True)
+    hashed_password: Mapped[str] = mapped_column()
